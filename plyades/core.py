54df6a68cd12f6f92dc4143580b8b92a49a8a133
Helge Eichhorn <mail@helgeeichhorn.de>
2012-12-19 12:51:03 +0100

    Prepare core.py for deletion.

--- plyades/core.py
+++ plyades/core.py
@@ -24,7 +24,7 @@ class Epoch(datetime.datetime):
 		return self.jd - 2400000.5
 
 class State:
-	def __init__(self, x, y, z, vx, vy, vz, epoch=Epoch(2000,1,1,0,0,0)):
+	def __init__(self, x, y, z, vx, vy, vz, epoch=Epoch(2000,1,1,0,0,0), frame="MEE2000"):
 		self.r = np.array([x, y, z])
 		self.v = np.array([vx, vy, vz])
 		self.t = epoch
