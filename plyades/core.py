595555433d7495ab54cdeb26d37cb2bc6c58f830
Helge Eichhorn <mail@helgeeichhorn.de>
2012-12-19 11:45:56 +0100

    Change julian date to be a property.

--- plyades/core.py
+++ plyades/core.py
@@ -2,20 +2,26 @@ import datetime
 import numpy as np
 
 class Epoch(datetime.datetime):
-	def get_jd(self, epoch=2000):
+	@property
+	def jd(self):
 		jd = (367.0 * self.year
 			 - np.floor( (7 * (self.year + np.floor( (self.month + 9) / 12.0) ) ) * 0.25 )
              + np.floor( 275 * self.month / 9.0 )
              + self.day + 1721013.5
              + ( (self.second/60.0 + self.minute ) / 60.0 + self.hour ) / 24.0)
-		if epoch == 2000:
-			return jd - 2451544.5
-		elif epoch == 1950:
-			return jd - 2433282.5
-		elif epoch == "mjd":
-			return jd - 2400000.5
-		elif epoch == 0:
-			return jd
+		return jd
+
+	@property
+	def jd2000(self):
+		return self.jd - 2451544.5
+
+	@property
+	def jd1950(self):
+		return self.jd - 2433282.5
+
+	@property
+	def mjd(self):
+		return self.jd - 2400000.5
 
 class State:
 	def __init__(self, x, y, z, vx, vy, vz, epoch=Epoch(2000,1,1,0,0,0)):
