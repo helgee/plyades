a03584301be09cf224a2949d584a0b3dc1f73258
Helge Eichhorn <mail@helgeeichhorn.de>
2013-01-30 16:14:18 +0100

    Add unittests for state vector from elements calc.

--- plyades/tests/test_orbit.py
+++ plyades/tests/test_orbit.py
@@ -4,26 +4,30 @@ import numpy as np
 import plyades as pl
 
 
-VALUES = {"vector": np.array([6524.834, 6862.875, 6448.296, 4.901327, 5.533756, -1.976341]),
+values = {"vector": np.array([6524.834, 6862.875, 6448.296, 4.901327, 5.533756, -1.976341]),
           "elements": np.array([36127.343, 0.832853, np.radians(87.870), np.radians(227.89),
                                 np.radians(53.38), np.radians(92.335)])}
 
 
 class OrbitTest(unittest.TestCase):
     def test_elements(self):
-        elements = pl.orbit.elements(VALUES["vector"], pl.const.EARTH["mu"])
+        elements = pl.orbit.elements(values["vector"], pl.constants.earth.mu)
         for i in range(len(elements)):
-            self.assertAlmostEqual(elements[i], VALUES["elements"][i], delta=1e-2)
+            self.assertAlmostEqual(elements[i], values["elements"][i], delta=1e-2)
 
     def test_elements_vectorized(self):
-        elements = pl.orbit.elements(np.vstack((VALUES["vector"], VALUES["vector"])), pl.const.EARTH["mu"])
+        elements = pl.orbit.elements(np.vstack((values["vector"], values["vector"])), pl.constants.earth.mu)
         for el in elements:
             for i in range(len(el)):
-                self.assertAlmostEqual(el[i], VALUES["elements"][i], delta=1e-2)
+                self.assertAlmostEqual(el[i], values["elements"][i], delta=1e-2)
 
+    def test_vector(self):
+        vector = pl.orbit.vector(values["elements"], pl.constants.earth.mu)
+        for i in range(len(vector)):
+            self.assertAlmostEqual(vector[i], values["vector"][i], delta=1e-2)
 
-    # def test_vector(self):
-    #     self.assertAlmostEqual(pl.orbit.vector(VALUES["elements"], pl.const.EARTH['mu']), VALUES["vector"])
-
-    # def test_vector_vectorized(self):
-    #     self.assertAlmostEqual(pl.orbit.vector(np.vstack((VALUES["elements"], VALUES["elements"])), pl.const.EARTH['mu']), np.vstack((VALUES["vector"], VALUES["vector"])))
+    def test_vector_vectorized(self):
+        vector = pl.orbit.vector(np.vstack((values["elements"], values["elements"])), pl.constants.earth.mu)
+        for v in vector:
+            for i in range(len(v)):
+                self.assertAlmostEqual(v[i], values["vector"][i], delta=1e-2)
