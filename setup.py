c0a9f6fc60ece5daae76c213a9d1997be49c4c45
Helge Eichhorn <mail@helgeeichhorn.de>
2013-03-26 14:13:06 +0100

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Plyades",
+      version="0.0.1",
+      description="A Python Astrodynamics Library",
+      author="Helge Eichhorn",
+      author_email="mail@helgeeichhorn.de",
+      url="https://github.com/helgee/plyades",
+      packages=["plyades", "plyades.constants", "plyades.tests"],
+      license="MIT License",
+      )
