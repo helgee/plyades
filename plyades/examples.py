db6b04a4053ce308c4831b581a8d2d59a0498fd4
Helge Eichhorn <mail@helgeeichhorn.de>
2013-04-29 23:37:11 +0200

    Tidy up examples.

--- plyades/examples.py
+++ plyades/examples.py
@@ -3,5 +3,14 @@ import datetime
 import numpy as np
 from core import State, Orbit
 
-iss = State(np.array([8.59072560e+02, -4.13720368e+03,  5.29556871e+03, 7.37289205e+00,   2.08223573e+00,   4.39999794e-01]), t=datetime.datetime(2013, 3, 18, 12, 0), body="Earth", frame="MEE2000")
+iss_state = np.array([
+    8.59072560e+02,
+    -4.13720368e+03,
+    5.29556871e+03,
+    7.37289205e+00,
+    2.08223573e+00,
+    4.39999794e-01,
+    ])
+iss_epoch = datetime.datetime(2013, 3, 18, 12, 0)
+iss = State(iss_state, t=iss_epoch, body="Earth", frame="MEE2000")
 
