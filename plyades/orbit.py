acd8fdcbeead8cd56a77b09b5eca9d62641f64be
Helge Eichhorn <git@helgeeichhorn.de>
2015-05-06 17:34:39 +0200

    Make orbits compatible with Astropy units.

--- plyades/orbit.py
+++ plyades/orbit.py
@@ -8,15 +8,15 @@ def rhs(s, t):
     pass
 
 
-def elements(vector, mu):
-    r = np.atleast_2d(vector)[:, 0:3]
-    v = np.atleast_2d(vector)[:, 3:6]
+def elements(mu, r, v):
+    r = np.atleast_2d(r)
+    v = np.atleast_2d(v)
     r_mag = util.mag(r)
     v_mag = util.mag(v)
-    h = np.cross(r, v)
+    h = util.cross(r, v)
     h_mag = util.mag(h)
     k = np.array([[0, 0, 1]]).repeat(r.shape[0], axis=0)
-    n = np.cross(k, h)
+    n = util.cross(k, h)
     n_mag = util.mag(n)
     xi = v_mag ** 2 / 2 - mu / r_mag
     e = ((v_mag ** 2 - mu / r_mag) * r - v * util.dot(r, v)) / mu
@@ -32,42 +32,25 @@ def elements(vector, mu):
     peri = np.arccos(util.dot(n, e) / (ecc * n_mag))
     ano = np.arccos(util.dot(e, r) / (ecc * r_mag))
     # Quadrant checks
-    n_ix = n[:, 1] < 0
-    if n_ix.any():
-        node[n_ix] = 2 * np.pi - node[n_ix]
-    p_ix = e[:, 2] < 0
-    if p_ix.any():
-        peri[p_ix] = 2 * np.pi - peri[p_ix]
-    a_ix = util.dot(r, v) < 0
-    if a_ix.any():
-        ano[a_ix] = 2 * np.pi - ano[a_ix]
-    # Return a 1D vector if the input was 1-dimensional.
-    if vector.shape == (6, ):
-        return np.hstack((sma, ecc, inc, node, peri, ano)).flatten()
-    else:
-        return np.hstack((sma, ecc, inc, node, peri, ano))
+    node = util.mod2pi(node)
+    peri = util.mod2pi(peri)
+    ano = util.mod2pi(ano)
+    return (
+        sma.squeeze(), ecc.squeeze(), inc.squeeze(),
+        node.squeeze(), peri.squeeze(), ano.squeeze())
 
 
 def print_elements(ele):
-    names = ["Semi-major axis [km]:", "Eccentricity:", "Inclination [deg]:",
-             "Ascending node [deg]:", "Argument of perigee [deg]:",
-             "True anomaly [deg]:"]
-    elements = np.append(ele[0:2], np.degrees(ele[2:6]))
-
-    strings = [
-        "{:<26}{:>16.5f}".format(name, element)
-        for name, element in zip(names, elements)]
-    print("\n".join(strings))
+    names = ["Semi-major axis:", "Eccentricity:", "Inclination:",
+             "Ascending node:", "Argument of perigee:",
+             "True anomaly:"]
+    for name, element in zip(names[:2], ele[:2]):
+        print("{:<26}{:>16.5f}".format(name, element))
+    for name, element in zip(names[2:], ele[2:]):
+        print("{:<26}{:>16.5f}".format(name, np.degrees(element)))
 
 
-def vector(elements, mu):
-    ele = np.atleast_2d(elements)
-    sma = ele[:, 0, np.newaxis]
-    ecc = ele[:, 1, np.newaxis]
-    inc = ele[:, 2, np.newaxis]
-    node = ele[:, 3, np.newaxis]
-    peri = ele[:, 4, np.newaxis]
-    ano = ele[:, 5, np.newaxis]
+def vector(mu, sma, ecc, inc, node, peri, ano):
     u = peri + ano
 
     p = sma * (1 - np.square(ecc))
@@ -88,10 +71,9 @@ def vector(elements, mu):
         vr*(np.sin(node)*np.cos(u) + np.cos(node)*np.cos(inc)*np.sin(u)) -
         vf*(np.sin(node)*np.sin(u) - np.cos(node)*np.cos(u)*np.cos(inc)))
     vz = vr*np.sin(inc)*np.sin(u) + vf*np.cos(u)*np.sin(inc)
-    if elements.shape == (6, ):
-        return np.hstack((x, y, z, vx, vy, vz)).flatten()
-    else:
-        return np.hstack((x, y, z, vx, vy, vz))
+    return (
+        x.squeeze(), y.squeeze(), z.squeeze(),
+        vx.squeeze(), vy.squeeze(), vz.squeeze())
 
 
 def period(a, mu):
--- plyades/util.py
+++ plyades/util.py
@@ -3,72 +3,73 @@
 from __future__ import division
 import re
 import numpy as np
+import astropy.units as u
 
 def rot(angle, axis=3):
-	'''
+    '''
 
-	'''
-	M = np.zeros((3,3))
-	if axis == 1:
-		M[0,0] = 1
-		M[1,1] = np.cos(angle)
-		M[1,2] = np.sin(angle)
-		M[2,1] = -np.sin(angle)
-		M[2,2] = np.cos(angle)
-		return M
-	elif axis == 2:
-		M[0,0] = np.cos(angle)
-		M[0,2] = -np.sin(angle)
-		M[1,1] = 1
-		M[2,0] = np.sin(angle)
-		M[2,2] = np.cos(angle)
-		return M
-	elif axis == 3:
-		M[0,0] = np.cos(angle)
-		M[0,1] = np.sin(angle)
-		M[1,0] = -np.sin(angle)
-		M[1,1] = np.cos(angle)
-		M[2,2] = 1
-		return M
+    '''
+    M = np.zeros((3,3))
+    if axis == 1:
+        M[0,0] = 1
+        M[1,1] = np.cos(angle)
+        M[1,2] = np.sin(angle)
+        M[2,1] = -np.sin(angle)
+        M[2,2] = np.cos(angle)
+        return M
+    elif axis == 2:
+        M[0,0] = np.cos(angle)
+        M[0,2] = -np.sin(angle)
+        M[1,1] = 1
+        M[2,0] = np.sin(angle)
+        M[2,2] = np.cos(angle)
+        return M
+    elif axis == 3:
+        M[0,0] = np.cos(angle)
+        M[0,1] = np.sin(angle)
+        M[1,0] = -np.sin(angle)
+        M[1,1] = np.cos(angle)
+        M[2,2] = 1
+        return M
 
 def rotd(angle, angular_velocity, axis=3):
-	M = np.zeros((3,3))
-	if axis == 1:
-		M[1,1] = -angular_velocity * np.sin(angle)
-		M[1,2] = angular_velocity * np.cos(angle)
-		M[2,1] = -angular_velocity * np.cos(angle)
-		M[2,2] = -angular_velocity * np.sin(angle)
-		return M
-	elif axis == 2:
-		M[0,0] = -angular_velocity * np.sin(angle)
-		M[0,2] = -angular_velocity * np.cos(angle)
-		M[2,0] = angular_velocity * np.cos(angle)
-		M[2,2] = -angular_velocity * np.sin(angle)
-		return M
-	elif axis == 3:
-		M[0,0] = -angular_velocity * np.sin(angle)
-		M[0,1] = angular_velocity * np.cos(angle)
-		M[1,0] = -angular_velocity * np.cos(angle)
-		M[1,1] = -angular_velocity * np.sin(angle)
-		return M
-		
+    M = np.zeros((3,3))
+    if axis == 1:
+        M[1,1] = -angular_velocity * np.sin(angle)
+        M[1,2] = angular_velocity * np.cos(angle)
+        M[2,1] = -angular_velocity * np.cos(angle)
+        M[2,2] = -angular_velocity * np.sin(angle)
+        return M
+    elif axis == 2:
+        M[0,0] = -angular_velocity * np.sin(angle)
+        M[0,2] = -angular_velocity * np.cos(angle)
+        M[2,0] = angular_velocity * np.cos(angle)
+        M[2,2] = -angular_velocity * np.sin(angle)
+        return M
+    elif axis == 3:
+        M[0,0] = -angular_velocity * np.sin(angle)
+        M[0,1] = angular_velocity * np.cos(angle)
+        M[1,0] = -angular_velocity * np.cos(angle)
+        M[1,1] = -angular_velocity * np.sin(angle)
+        return M
+
 def euler(alpha, beta, gamma, order="321"):
-	
-	# Input checking.
-	reg = re.compile("[1-3][1-3][1-3]")
-	if not reg.match(order) and len(order) > 3:
-		raise ValueError("Incorrect rotation order definition.")
-	
-	m_alpha = rot(alpha, axis=int(order[0]))
-	m_beta = rot(beta, axis=int(order[1]))
-	m_gamma = rot(gamma, axis=int(order[2]))
-	return np.dot(m_alpha, np.dot(m_beta, m_gamma))
+
+    # Input checking.
+    reg = re.compile("[1-3][1-3][1-3]")
+    if not reg.match(order) and len(order) > 3:
+        raise ValueError("Incorrect rotation order definition.")
+
+    m_alpha = rot(alpha, axis=int(order[0]))
+    m_beta = rot(beta, axis=int(order[1]))
+    m_gamma = rot(gamma, axis=int(order[2]))
+    return np.dot(m_alpha, np.dot(m_beta, m_gamma))
 
 def dms2rad(degrees, minutes, seconds):
-	return (degrees + minutes/60 + seconds/3600) * np.pi/180
+    return (degrees + minutes/60 + seconds/3600) * np.pi/180
 
 def hms2rad(degrees, minutes, seconds):
-	return (degrees + minutes/60 + seconds/3600) * 15 * np.pi/180
+    return (degrees + minutes/60 + seconds/3600) * 15 * np.pi/180
 
 def mag(a):
     """Vectorized magnitude calculation for an array of vectors.
@@ -99,7 +100,29 @@ def dot(a, b):
     Returns
     -------
     m: numpy.ndarray
-        An mx1-array containing of a*b for each vector pair. 
+        An mx1-array containing of a*b for each vector pair.
     """
     return (a * b).sum(axis=1, keepdims=True)
 
+def getunit(val):
+    return getattr(val, "unit", None)
+
+def cross(a, b):
+    unit_a = getunit(a)
+    unit_b = getunit(b)
+    if not unit_a and not unit_b:
+        return np.cross(a, b)
+    else:
+        if not unit_a:
+            unit_a = u.dimensionless_unscaled
+        if not unit_b:
+            unit_b = u.dimensionless_unscaled
+        return np.cross(a, b)*unit_a*unit_b
+
+
+def mod2pi(val):
+    unit = getunit(val)
+    if not unit:
+        return np.mod(val, 2*np.pi)
+    else:
+        return np.mod(val, 2*np.pi*unit)
