41dd6bc377ce850d3b0e73684beaff63315d8371
Helge Eichhorn <git@helgeeichhorn.de>
2015-05-06 17:35:15 +0200

    Adjust example.

--- plyades/examples.py
+++ plyades/examples.py
@@ -2,6 +2,7 @@ from __future__ import division
 import datetime
 import numpy as np
 from plyades.core import State, Orbit
+from plyades.bodies import EARTH
 
 iss_state = np.array([
     8.59072560e+02,
@@ -12,5 +13,4 @@ iss_state = np.array([
     4.39999794e-01,
     ])
 iss_epoch = datetime.datetime(2013, 3, 18, 12, 0)
-iss = State(iss_state, t=iss_epoch, body="Earth", frame="MEE2000")
-
+iss = State(iss_state, t=iss_epoch, body=EARTH, frame="MEE2000")
